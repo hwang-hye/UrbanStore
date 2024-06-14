a5fb9bf752afa7a1cfda116ed92cb2e6c53d4a97
hwanghye <hwang.sine@gmail.com>
2024-06-14 19:12:07 +0900

    feat: Sign Start UI 구현

--- UrbanStore/SceneDelegate.swift
+++ UrbanStore/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         
-        let viewController = ViewController()
+        let viewController = SignViewController()
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
         
---
+++ UrbanStore/Sign/SignViewController.swift
@@ -0,0 +1,68 @@
+//
+//  SignViewController.swift
+//  UrbanStore
+//
+//  Created by hwanghye on 6/14/24.
+//
+
+import UIKit
+import SnapKit
+
+class SignViewController: UIViewController {
+    
+    let titleLabel = UILabel()
+    let titleImageView = UIImageView()
+    var startButton = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+        configureLayout()
+    
+    }
+    
+    
+    func configure() {
+        titleLabel.text = "URBAN STORE"
+        titleLabel.font = .systemFont(ofSize: 36, weight: .bold)
+        titleLabel.textAlignment = .center
+        
+        titleImageView.image = UIImage(named: "launch")
+        titleImageView.contentMode = .scaleAspectFill
+        
+        startButton.setTitle("시작하기", for: .normal)
+        startButton.setTitleColor(.white, for: .normal)
+        startButton.setTitleColor(.gray, for: .highlighted)
+        startButton.layer.backgroundColor = UIColor.black.cgColor
+        startButton.layer.cornerRadius = 22
+        startButton.titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
+    }
+    
+    
+    func configureLayout() {
+        view.backgroundColor = .white
+        
+        view.addSubview(titleLabel)
+        view.addSubview(titleImageView)
+        view.addSubview(startButton)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(100)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(50)
+            make.height.equalTo(44)
+        }
+        
+        titleImageView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(80)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(50)
+            make.height.equalTo(300)
+        }
+        
+        startButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(30)
+            make.height.equalTo(44)
+        }
+    }
+}
